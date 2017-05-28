e99185a3c69d9ce39fa68932ea122304cb5f1c78
juan <chavezvasquezjuan@gmail.com>
2017-05-28 17:01:07 -0500

    main layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!-- sidebar-area -->
+<div class="col-sm-4 col-md-3 sidebar-area">
+    <!-- categries widget -->
+    <div class="categories-sidebar-widget widget no-border">
+        <h5 class="widget-title">Categories</h5>
+
+        <p class="product-category">
+            <a href="#">Fashion</a>
+        </p><!-- / category -->
+
+        <p class="product-category">
+            <a href="#">Furniture</a>
+        </p><!-- / category -->
+
+        <p class="product-category">
+            <a href="#">Art</a>
+        </p><!-- / category -->
+    </div>
+    <!-- / categories-sidebar-widget -->
+</div><!-- / sidebar-area -->
