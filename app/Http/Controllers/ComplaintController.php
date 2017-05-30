00a69c8373878c9eff148eab897b370845cb6cbe
takeshi <takeshi.farro@gmail.com>
2017-05-29 23:20:56 -0500

    complaint controller

---
+++ app/Http/Controllers/ComplaintController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Complaint;
+
+class ComplaintController extends Controller
+{
+    public function create($complaint){
+
+    }
+}
