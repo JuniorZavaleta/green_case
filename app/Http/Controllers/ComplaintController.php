3e592af2760c309fd4f9c69baa870faf26b7561c
takeshi <takeshi.farro@gmail.com>
2017-05-30 00:39:02 -0500

    changes in create and store functions

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -7,7 +7,30 @@ use App\Models\Complaint;
 
 class ComplaintController extends Controller
 {
-    public function create($complaint){
-
+    public function create()
+    {
+        return view('complaints.new');
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'citizen_id',
+            'authority_id',
+            'type_contamination_id',
+            'type_communication_id'
+            'complaint_state_id',
+            'latitude',
+            'longitude',
+            'commentary'
+        ]);
+
+      $citizen = Citizen::create([
+                'name' => $facebook_user->user['first_name']
+            ]);
+
+      $area->save();
+
+      return redirect()->route('areas.index')->with('message', 'registered area');
+   }
 }
