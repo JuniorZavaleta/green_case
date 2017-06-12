5cf8340aee3e1133e6779fdcdf70655c649d9638
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-06-12 19:51:38 +0000

    interface for messenger senders and telegram implementation not tested yet

---
+++ app/Services/MessengerInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services;
+
+interface MessengerInterface
+{
+    function sendMessage($receiver, $subject, $view, $data);
+}
---
+++ app/Services/Telegram.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+class TelegramApiException extends \Exception
+{
+    public function __construct()
+    {
+        parent::__construct("Inconvenientes con la Api de Telegram.");
+    }
+}
+
+class Telegram implements MessengerInterface
+{
+    /**
+     * Base url Telegram Api Bot
+     * @var string
+     */
+    protected $base_url;
+
+    /**
+     * Set the base url to use on sendMessage
+     */
+    public function __construct()
+    {
+        $this->base_url = 'https://api.telegram.org/bot'.env('TELEGRAM_BOT_TOKEN');
+    }
+
+    public function sendMessage($receiver, $subject, $view, $data)
+    {
+        $message = view($view)->render();
+        $url = "{$this->base_url}/sendMessage?text={$message}&chat_id={$receiver}";
+        $result = json_decode(file_get_contents($url), true);
+
+        if (isset($result['ok']) && $result['ok']) {
+            return true;
+        }
+
+        throw new \TelegramApiException;
+    }
+}
