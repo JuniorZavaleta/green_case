982888cd3295e6d636b71f83b9289445708d270f
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-06-15 19:18:26 +0000

    get more complaints by ws

--- app/Http/Controllers/App/IndexController.php
+++ app/Http/Controllers/App/IndexController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers\App;
 
 use App\Http\Controllers\Controller;
+use DB;
 
 use App\Models\Complaint;
 
 class IndexController extends Controller
 {
     /**
-     * List the last 10 complaints completed
+     * List the last 15 complaints completed (use default)
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $complaints = Complaint::completed()->latest()->paginate(10);
+        $complaints = Complaint::completed()->latest('id')->paginate();
 
         if (!session()->has('show_support_message')) {
             session(['show_support_message' => true]);
@@ -28,5 +29,20 @@ class IndexController extends Controller
         session(['show_support_message' => false]);
 
         return response()->json(['response' => 'ok'], 200);
+    }
+
+    public function nextComplaints()
+    {
+        $complaints = Complaint::completed()->latest('id')->paginate();
+
+        return $complaints->map(function ($complaint) {
+            return [
+                'id' => $complaint->id,
+                'from' => $complaint->channel->description,
+                'type' => $complaint->contamination_type->description,
+                'created_at' => $complaint->created_at_formatted,
+                'images' => $complaint->images()->pluck('img'),
+            ];
+        });
     }
 }
