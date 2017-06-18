3df05fadf7ae02f13c45dd50bdcbed154df44566
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-06-18 14:36:55 -0500

    remove audio and video preview js

--- resources/views/admin/activity/create.blade.php
+++ resources/views/admin/activity/create.blade.php
@@ -148,11 +148,6 @@
 <script src="{{ asset('/js/jquery.fileupload-process.js') }}"></script>
 <!-- The File Upload image preview & resize plugin-->
 <script src="{{ asset('/js/jquery.fileupload-image.js') }}"></script>
-<!-- The File Upload audio preview plugin-->
-<script src="{{ asset('/js/jquery.fileupload-audio.js') }}"></script>
-<!-- The File Upload video preview plugin-->
-<script src="{{ asset('/js/jquery.fileupload-video.js') }}"></script>
-<!-- The File Upload validation plugin-->
 <script src="{{ asset('/js/jquery.fileupload-validate.js') }}"></script>
 <!-- The File Upload user interface plugin-->
 <script src="{{ asset('/js/jquery.fileupload-ui.js') }}"></script>
