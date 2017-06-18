db7e254d0edfa017c22b5ba3425f6f29ee1e10ff
takeshifh <takeshi.farro@gmail.com>
2017-06-18 11:55:23 -0500

    validation in spanish

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -37,7 +37,7 @@ class ComplaintController extends Controller
     public function store(ImageUpload $image_uploader)
     {
         $this->validate(request(), [
-            'type_contamination' => 'integer|required',
+            'contamination_type' => 'required',
             'latitude'           => 'required',
             'longitude'          => 'required',
             'commentary'         => 'string',
