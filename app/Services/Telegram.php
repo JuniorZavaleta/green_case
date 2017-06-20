1e9eecf7f9bf827eb9b0e1100535bc846c91c9e5
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-06-19 20:30:59 -0500

    telegram notificatio new activity

--- app/Services/Telegram.php
+++ app/Services/Telegram.php
@@ -30,13 +30,66 @@ class Telegram implements MessengerInterface
     {
         $view = 'notifications.telegram.'.$view;
         $message = view($view, $data)->render();
-        $url = "{$this->base_url}/sendMessage?text={$message}&chat_id={$receiver}";
-        $result = json_decode(file_get_contents($url), true);
+
+        $url = "{$this->base_url}/sendMessage?chat_id={$receiver}";
+
+        $post_fields = [
+            'chat_id' => $receiver,
+            'text' => $message,
+            'parse_mode' => 'Markdown',
+        ];
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+
+        $result = json_decode(curl_exec($ch), true);
+
+        curl_close($ch);
+
+        if (isset($result['ok']) && $result['ok']) {
+            if (array_key_exists('images', $data)) {
+                foreach ($data['images'] as $image) {
+                    $this->sendImage($receiver, $image->img);
+                }
+            }
+
+            return true;
+        }
+
+        throw new TelegramApiException;
+    }
+
+    private function getRealPath($image)
+    {
+        $img_pos = strpos($image, 'img');
+
+        return public_path(substr($image, $img_pos));
+    }
+
+    public function sendImage($receiver, $image)
+    {
+        $url = "{$this->base_url}/sendPhoto?chat_id={$receiver}";
+
+        $post_fields = [
+            'chat_id' => $receiver,
+            'photo' => new \CURLFile($this->getRealPath($image)),
+        ];
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type:multipart/form-data"
+        ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+        $result = curl_exec($ch);
 
         if (isset($result['ok']) && $result['ok']) {
             return true;
         }
 
+        \Log::error(curl_error($ch));
         throw new TelegramApiException;
     }
 }
---
+++ resources/views/notifications/telegram/new_activity.blade.php
@@ -0,0 +1,10 @@
+*¡Tenemos noticias!*
+
+_Se ha realizado una nueva actividad sobre tu caso de contaminación_
+
+```text
+{{ $activity->title }}:
+{{ $activity->description }}
+```
+
+Te dejamos algunas imágenes sobre la actividad realizada
